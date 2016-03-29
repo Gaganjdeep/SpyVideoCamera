f4ed728b3eb7265ad338e1faad1d61764c823769
gagandeep <gagandeep.singh@amebasoftwares.com>
2016-03-29 14:34:10 +0530

    Initial commit

---
+++ app/src/main/java/gagan/ameba/spycamera/NotificationHelper.java
@@ -0,0 +1,83 @@
+package gagan.ameba.spycamera;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.NotificationCompat;
+
+import java.io.IOException;
+
+/**
+ * Created by gagandeep on 28 Mar 2016.
+ */
+public class NotificationHelper
+{
+
+
+    public NotificationHelper(Context context, Uri ImageUri)
+    {
+        showNotification(context, ImageUri);
+
+    }
+
+    private void showNotification(Context context, Uri imagUri)
+    {
+//        Intent notificationIntent = new Intent(context, MainActivityG.class);
+//        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(
+                "content://media/internal/images/media"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+
+        builder.setContentIntent(pendingIntent);
+
+        builder.setAutoCancel(true);
+
+//        builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.no_img));
+
+        try
+        {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), imagUri);
+//        }
+            builder.setLargeIcon(bitmap);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        builder.setContentTitle("SpyCam");
+        builder.setContentText("Image Captured ! Click to open!");
+
+
+        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+
+        if (defaultSound == null)
+        {
+            defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            if (defaultSound == null)
+            {
+                defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+            }
+        }
+
+        builder.setSound(defaultSound);
+
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(
+                Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, builder.build());
+    }
+}
---
+++ app/src/main/java/gagan/ameba/spycamera/ReceiverG.java
@@ -0,0 +1,32 @@
+package gagan.ameba.spycamera;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+import gagan.ameba.spycamera.service_background.CamerService;
+import gagan.ameba.spycamera.service_background.SharedPrefHelper;
+
+/**
+ * Created by gagandeep on 23 Mar 2016.
+ */
+public class ReceiverG extends WakefulBroadcastReceiver
+{
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+//        if (intent.getAction().equals("android.media.VOLUME_CHANGED_ACTION"))
+//        {
+
+        if (new SharedPrefHelper(context).isCaptureOnUnlock())
+        {
+            Intent front_translucent = new Intent(context, CamerService.class);
+            front_translucent.putExtra("Front_Request", true);
+            front_translucent.putExtra("Quality_Mode", 0);
+            context.startService(
+                    front_translucent);
+        }
+
+    }
+}
