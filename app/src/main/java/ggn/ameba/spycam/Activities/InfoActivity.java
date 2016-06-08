65247145e97600251e72441d073afcc67edc3e74
gagandeep <gagandeep.singh@amebasoftwares.com>
2016-06-08 12:30:24 +0530

    language support

---
+++ app/src/main/java/ggn/ameba/spycam/Activities/InfoActivity.java
@@ -0,0 +1,17 @@
+package ggn.ameba.spycam.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ggn.ameba.spycam.R;
+
+public class InfoActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
